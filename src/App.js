8c677a4e9561715b7bed68f948c91f51d4fb159c
Philippe Maman <pmaman@users.noreply.github.com>
2024-05-03 12:01:35 -0700

    not using browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ResponsiveAppBar from './Components/Nav';
 import About from './Components/About.js';
 import Atlas from './Components/Atlas.js';
